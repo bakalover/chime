cd780af39b931dbc5b599f50f8f91bd5470ef480
bakalover <kostya@shprenger.com>
2023-10-28 14:08:38 +0300

    QSpin

---
+++ support/locks/spinlock.hpp
@@ -0,0 +1,35 @@
+#include <atomic>
+
+class SpinLock
+{
+public:
+    class Guard
+    {
+        friend class SpinLock;
+
+    public:
+        explicit Guard(SpinLock &host);
+
+        ~Guard();
+
+    private:
+        SpinLock &host_;
+        std::atomic<Guard *> next_{nullptr};
+        std::atomic<bool> is_owner_{false};
+    };
+
+    SpinLock(){};
+
+private:
+    void Acquire(Guard *waiter);
+    void Release(Guard *owner);
+    void CompleteLink(Guard *prev, Guard *next);
+    void WaitForAccess(Guard *waiter);
+    void WaitForLink(Guard *owner);
+    void SignalToNext(Guard *owner);
+    bool IsChainLeader(Guard *waiter);
+    bool HaveNextWaiter(Guard *owner);
+
+private:
+    std::atomic<Guard *> tail_{nullptr};
+};
