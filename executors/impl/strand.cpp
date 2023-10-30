0495aa7f0c48bd34b46e8c874dce37a4a0042117
bakalover <kostya@shprenger.com>
2023-10-30 15:30:55 +0300

    Added Strand

---
+++ executors/impl/strand.cpp
@@ -0,0 +1,41 @@
+
+#include <cstddef>
+#include <meijic/executors/impl/strand.hpp>
+namespace exec {
+
+Strand::Strand(Executor &underlying)
+    : underlying_(underlying), queue_(), spinlock_() {}
+
+void Strand::Submit(Task *task) {
+  {
+    sup::SpinLock::Guard guard(spinlock_);
+    queue_.push(std::move(task));
+  }
+  if (count_.fetch_add(1) == 0) {
+    SubmitSelf();
+  }
+}
+
+void Strand::Run() noexcept {
+  const size_t done = RunTasks();
+  const size_t left = count_.fetch_sub(done);
+  if (left > done) {
+    SubmitSelf();
+  }
+}
+
+size_t Strand::RunTasks() {
+  size_t done = 0;
+  {
+    sup::SpinLock::Guard guard(spinlock_);
+    while (!queue_.empty()) {
+      queue_.front()->Run();
+      queue_.pop();
+      ++done;
+    }
+  }
+  return done;
+}
+// TODO: Lifetime problems
+void Strand::SubmitSelf() { underlying_.Submit(this); }
+} // namespace exec
---
+++ executors/impl/strand.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "meijic/executors/task.hpp"
+#include <atomic>
+#include <meijic/executors/executor.hpp>
+#include <meijic/support/locks/spinlock.hpp>
+#include <queue>
+
+namespace exec {
+class Strand : public Executor, Task {
+public:
+  explicit Strand(Executor &underlying);
+
+  // Non-copyable
+  Strand(const Strand &) = delete;
+  Strand &operator=(const Strand &) = delete;
+
+  // Non-movable
+  Strand(Strand &&) = delete;
+  Strand &operator=(Strand &&) = delete;
+
+  void Submit(Task *task) override;
+
+private:
+  void SubmitSelf();
+  void Run() noexcept override;
+  size_t RunTasks();
+
+private:
+  exec::Executor &underlying_;
+  std::atomic<size_t> count_{0};
+  // TODO: Transfer to MPSCQueue (Lock-free!!)
+  std::queue<Task *> queue_;
+  sup::SpinLock spinlock_;
+};
+
+} // namespace exec
