73d52ce353eeb75a91d0adb71cf0fc9c19e1ec0a
bakalover <kostya@shprenger.com>
2023-11-14 21:34:05 +0300

    Yeild Awaiter

--- meijic/fibers/sched/yield.cpp
+++ meijic/fibers/sched/yield.cpp
@@ -1,10 +1,15 @@
-// #include <meijic/fibers/sched/yield.hpp>
-// #include <meijic/fibers/fiber.hpp>
-// namespace fib::sched {
-// void Yield() {
-//   Fiber *cur_fiber = Fiber::Self();
-//   if (cur_fiber != nullptr) {
-//     cur_fiber->Yield();
-//   }
-// }
-// } // namespace fibers
+#include <meijic/fibers/fiber.hpp>
+#include <meijic/fibers/sched/yield.hpp>
+namespace fib::sched {
+struct YieldAwaiter : IAwaiter {
+  void AwaitSuspend(FiberHandle handle) override { handle.Schedule(); }
+};
+
+void Yield() {
+  YieldAwaiter awaiter;
+  auto cur_fiber = Fiber::Self();
+  if (cur_fiber != nullptr) {
+    cur_fiber->Suspend(&awaiter);
+  }
+}
+} // namespace fib::sched
