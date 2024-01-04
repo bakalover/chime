3b86539e4fbac3ac826c63d87b00209121fa8217
bakalover <kostya@shprenger.com>
2024-01-04 21:39:30 +0300

    Life base

---
+++ meijic/support/life/management.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <atomic>
+#include <cstddef>
+#include <twist/ed/stdlike/atomic.hpp>
+
+namespace support::life {
+template <typename T> struct LifeManagableBase {
+  void ShortenLife() {
+    if (life_counter.fetch_sub(1, std::memory_order::acq_rel) == 1) {
+      SelfDestroy();
+    }
+  }
+
+  void ExtendLife() { life_counter.fetch_add(1, std::memory_order::release); }
+
+  void SelfDestroy() { delete static_cast<T *>(this); }
+
+  twist::ed::stdlike::atomic<size_t> life_counter{1};
+};
+} // namespace support::life
