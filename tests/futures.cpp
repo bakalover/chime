fc257f05cefbc5ac845646b8f0ab2d665e57e8aa
bakalover <kostya@shprenger.com>
2024-01-13 14:09:25 +0300

    Nice test

--- tests/futures.cpp
+++ tests/futures.cpp
@@ -1,8 +1,13 @@
 #include "chime/fibers/sched/spawn.hpp"
 #include "chime/fibers/sched/yield.hpp"
+#include "chime/futures/combine/seq/map.hpp"
+#include "chime/futures/combine/seq/start.hpp"
+#include "chime/futures/combine/seq/via.hpp"
 #include <chime/executors/impl/pool.hpp>
 #include <chime/fibers/sched.hpp>
+#include <chime/futures/full_pack.hpp>
 #include <iostream>
+#include <utility>
 int main() {
   executors::Pool pool{1};
 
@@ -10,9 +15,13 @@ int main() {
 
   Spawn(pool, [&] {
     std::cout << "1\n";
-    Spawn(pool, [] {
+    Spawn(pool, [&] {
       std::cout << "2\n";
+      auto f = Just() | Via(pool) | Map([](result::Unit) { return "hello"; }) |
+               Bang();
       Yield();
+
+      std::cout << *(std::move(f) | Get()) << '\n';
       std::cout << "4\n";
     });
     Yield();
