f7a63fa8ce12df4647184c806d7ac9c15f634c71
bakalover <kostya@shprenger.com>
2024-03-24 13:32:18 +0300

    Flatten

--- chime/executors/full_pack.hpp
+++ chime/executors/full_pack.hpp
@@ -1,10 +1,10 @@
 #pragma once
 // Testing purposes
 #include <chime/executors/container.hpp>
-#include <chime/executors/impl/inline.hpp>
-#include <chime/executors/impl/manual.hpp>
-#include <chime/executors/impl/pool/pool.hpp>
-#include <chime/executors/impl/strand.hpp>
+#include <chime/executors/inline.hpp>
+#include <chime/executors/manual.hpp>
+#include <chime/executors/pool/pool.hpp>
+#include <chime/executors/strand.hpp>
 #include <chime/executors/submit.hpp>
 #include <chime/executors/task.hpp>
 using namespace executors;
